cf4ec2233c58d8c8f9d1c9a5783407650062bf0f
nakaji <nakaji126@gmail.com>
2011-04-28 11:09:02 +0900

    ViewFlipper内の子を取得し、OnClickListenerを割り当てる様にした

--- src/com/nakaji/android/flicksample/FlickSampleActivity.java
+++ src/com/nakaji/android/flicksample/FlickSampleActivity.java
@@ -1,25 +1,12 @@
 package com.nakaji.android.flicksample;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ListView;
 import android.widget.ViewFlipper;
 
 public class FlickSampleActivity extends Activity {
 
-    ListView listView01;
-    ListView listView02;
-    ListView listView03;
-
+    ViewFlipper viewFlipper;
 
     /** Called when the activity is first created. */
     @Override
@@ -27,99 +14,10 @@ public class FlickSampleActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        listView01 = (ListView) findViewById(R.id.ListView01);
-        listView02 = (ListView) findViewById(R.id.ListView02);
-        listView03 = (ListView) findViewById(R.id.ListView03);
+        viewFlipper = (ViewFlipper) findViewById(R.id.ViewFlipper);
 
-        FlickUtils futil = new FlickUtils();
-        futil.setOnTouchListener(this, new View[] { listView01, listView02, listView03 });
-    }
-
-    private class FlickUtils {
-        ViewFlipper viewFlipper;
-
-        GestureDetector gestureDetecotr;
-
-        Animation inFromLeft;
-        Animation outToRight;
-        Animation inFromRight;
-        Animation outToLeft;
-
-        // ジェスチャーリスナー
-        OnGestureListener gestureListener = new OnGestureListener() {
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public void onShowPress(MotionEvent e) {
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                return false;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent e) {
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                float dx = Math.abs(velocityX);
-                float dy = Math.abs(velocityY);
-                Log.d("Chapter06", "onFling : (" + dx + "," + dy + ")");
-                if (dx > dy && dx > 150) {
-                    if (e1.getX() < e2.getX()) {
-                        viewFlipper.setInAnimation(inFromLeft);
-                        viewFlipper.setOutAnimation(outToRight);
-                        viewFlipper.showPrevious();
-                    } else {
-                        viewFlipper.setInAnimation(inFromRight);
-                        viewFlipper.setOutAnimation(outToLeft);
-                        viewFlipper.showNext();
-                    }
-                    return true;
-                }
-
-                return false;
-            }
-
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return false;
-            }
-        };
-
-        // タッチ処理リスナー
-        OnTouchListener touchListener = new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Log.d("Chapter06", "onTouch : " + v.toString());
-                if (gestureDetecotr.onTouchEvent(event)) {
-                    return true;
-                }
-                return false;
-            }
-        };
-
-        public void setOnTouchListener(Context context, View[] views) {
-
-            viewFlipper = (ViewFlipper) findViewById(R.id.ViewFlipper);
-
-            gestureDetecotr = new GestureDetector(context, gestureListener);
-
-            inFromLeft = AnimationUtils.loadAnimation(context, R.anim.in_from_left);
-            outToRight = AnimationUtils.loadAnimation(context, R.anim.out_to_right);
-            inFromRight = AnimationUtils.loadAnimation(context, R.anim.in_from_right);
-            outToLeft = AnimationUtils.loadAnimation(context, R.anim.out_to_left);
-
-            for (View v : views) {
-                v.setOnTouchListener(touchListener);
-            }
-        }
-
+        FlickUtil futil = new FlickUtil();
+        futil.setOnTouchListener(this, viewFlipper);
     }
 
 }
---
+++ src/com/nakaji/android/flicksample/FlickUtil.java
@@ -0,0 +1,100 @@
+package com.nakaji.android.flicksample;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
+
+public class FlickUtil {
+    ViewFlipper viewFlipper;
+
+    GestureDetector gestureDetecotr;
+
+    Animation inFromLeft;
+    Animation outToRight;
+    Animation inFromRight;
+    Animation outToLeft;
+
+    // ジェスチャーリスナー
+    OnGestureListener gestureListener = new OnGestureListener() {
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float dx = Math.abs(velocityX);
+            float dy = Math.abs(velocityY);
+            Log.d("Chapter06", "onFling : (" + dx + "," + dy + ")");
+            if (dx > dy && dx > 150) {
+                if (e1.getX() < e2.getX()) {
+                    viewFlipper.setInAnimation(inFromLeft);
+                    viewFlipper.setOutAnimation(outToRight);
+                    viewFlipper.showPrevious();
+                } else {
+                    viewFlipper.setInAnimation(inFromRight);
+                    viewFlipper.setOutAnimation(outToLeft);
+                    viewFlipper.showNext();
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
+    };
+
+    // タッチ処理リスナー
+    OnTouchListener touchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Log.d("Chapter06", "onTouch : " + v.toString());
+            if (gestureDetecotr.onTouchEvent(event)) {
+                return true;
+            }
+            return false;
+        }
+    };
+
+    public void setOnTouchListener(Context context, ViewFlipper flipper) {
+
+        viewFlipper = flipper;
+        gestureDetecotr = new GestureDetector(context, gestureListener);
+
+        inFromLeft = AnimationUtils.loadAnimation(context, R.anim.in_from_left);
+        outToRight = AnimationUtils.loadAnimation(context, R.anim.out_to_right);
+        inFromRight = AnimationUtils.loadAnimation(context, R.anim.in_from_right);
+        outToLeft = AnimationUtils.loadAnimation(context, R.anim.out_to_left);
+
+        // FiewFlipperの子に対して、OnTouchListenerを割り当てる
+        int child_count = viewFlipper.getChildCount();
+        for (int i = 0; i < child_count; i++) {
+            viewFlipper.getChildAt(i).setOnTouchListener(touchListener);
+        }
+    }
+
+}
